7faac2439073a2de6e0a1b4252cee94dd5e0f6f6
inyee786 <intakhab.cusat@gmail.com>
2018-09-17 02:35:10 +0530

    comment

--- routes/mayaapiserver/listvolume.js
+++ routes/mayaapiserver/listvolume.js
@@ -24,44 +24,44 @@ var options1 = {
         'Content-Type': 'application/json'
     }
 };
-http.get(options1, function (err, resp, body) {
-    if (err) {
-        console.log("this is volume erro namespaces ");
-    } else {
-        data = JSON.parse(body);
-        console.log(JSON.stringify(data.items));
-        console.log("this is volume lis http");
-        console.log(data);
-    }
-});
+// http.get(options1, function (err, resp, body) {
+//     if (err) {
+//         console.log("this is volume erro namespaces ");
+//     } else {
+//         data = JSON.parse(body);
+//         console.log(JSON.stringify(data.items));
+//         console.log("this is volume lis http");
+//         console.log(data);
+//     }
+// });
 
-router.get('/volume', (req, res) => {
+// router.get('/volume', (req, res) => {
 
-    var options = {
-        url: `http://${process.argv[6]}:5656/latest/volumes/`,
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'namespace': `${process.argv[5]}`
-        }
-    };
+//     var options = {
+//         url: `http://${process.argv[6]}:5656/latest/volumes/`,
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'namespace': `${process.argv[5]}`
+//         }
+//     };
 
-    http.get(options, function (err, resp, body) {
-        if (err) {
-            //   reject(err);
-            console.log(err);
-            console.log("this is volume erro namespaces ");
-        } else {
-            data = JSON.parse(body);
-            console.log(data);
-            //   numberOfrepo = JSON.parse(body).length;
-            console.log("this is volume lis http");
-            //    console.log(resp);
-            console.log("this is volume lis http");
-        }
-        // console.log(body +' this is body1')
-        res.status(200).json({ data });
-    });
+//     http.get(options, function (err, resp, body) {
+//         if (err) {
+//             //   reject(err);
+//             console.log(err);
+//             console.log("this is volume erro namespaces ");
+//         } else {
+//             data = JSON.parse(body);
+//             console.log(data);
+//             //   numberOfrepo = JSON.parse(body).length;
+//             console.log("this is volume lis http");
+//             //    console.log(resp);
+//             console.log("this is volume lis http");
+//         }
+//         // console.log(body +' this is body1')
+//         res.status(200).json({ data });
+//     });
 
-});
+// });
 module.exports = router;
